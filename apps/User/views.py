42ddadd7ef85867bbb61092135ad02c4c706b66c
RobinInagan <rsinagano@correo.udistrital.edu.co>
2024-06-02 22:12:40 -0500

    +req4

--- apps/User/views.py
+++ apps/User/views.py
@@ -5,11 +5,12 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
 
 #Self Modules
 from .models import *
 from .serializers import *
+from apps.Restaurant.models import Order
+from apps.Restaurant.serializers import OrderSerializaerModel
 
 
 class UsersViewSet(ModelViewSet):
@@ -21,10 +22,6 @@ class WaiterViewSet(ModelViewSet):
         queryset = Waiter.objects.all()
         serializer_class = WaiterSerializerModel
 
-        permission_classes=[
-             IsAuthenticated
-        ]
-
         @action(detail=True, methods=['post'])
         def add_shift(self, request, pk=None):
             waiter = self.get_object()
@@ -35,6 +32,23 @@ class WaiterViewSet(ModelViewSet):
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            
+        def get_serializer_class(self):
+             print(self.request.query_params)
+             return super().get_serializer_class()
+        
+        @action(detail=True, methods=['get'])
+        def orders(self, request, pk=None):
+            waiter = self.get_object()
+            active = request.query_params.get('active')
+            if active:
+                orders = Order.objects.filter(waiter=waiter.id).filter(
+                    models.Q(bill__isnull=True) | models.Q(bill__final_cost__isnull=True)
+                )
+            else:
+                orders = Order.objects.filter(waiter=waiter.id)
+            serializer = OrderSerializaerModel(orders, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
 class WaiterShiftViewSet(ModelViewSet):
       queryset = Waiter_Shift.objects.all()
